cf66404703358bc56aecf4b93a3732b7379c26cb
DanielOch8a <fdaniel.ochoac@gmail.com>
2024-09-25 16:27:05 -0700

    Added coments and my name

--- 2_3_personal_message.py
+++ 2_3_personal_message.py
@@ -1,3 +1,5 @@
+#Francisco Daniel Ochoa Castillo S7A
+#This program print a greeting to a person with their name variable, and then print that he is learning GIT.
 name = "Danny"
 print(f"Hello {name}! Welcome to this new adventure on Python :D")
 print("Learning GIT!")
