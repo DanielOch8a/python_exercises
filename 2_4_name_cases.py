48258fc402462a13d4f432b72d1212f4f106293b
DanielOch8a <fdaniel.ochoac@gmail.com>
2024-09-25 16:27:49 -0700

    Added coments about the program and my name

--- 2_4_name_cases.py
+++ 2_4_name_cases.py
@@ -1,3 +1,5 @@
+#Francisco Daniel Ochoa Castillo S7A
+#This program is about formating strings in a print function with upper, lower and title
 name = "Francisco daniel Ochoa CASTILLO"
 print(f"Mi nombre es: {name.upper()}")
 print(f"Mi nombre es: {name.lower()}")
